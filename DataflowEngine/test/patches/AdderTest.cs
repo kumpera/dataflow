7c709e060776b2cf67407619890aefba141d9ead
Carlos Villela <cv@cv.local>
2008-04-08 07:58:16 +0100

    added test

--- DataflowEngine/test/patches/AdderTest.cs
+++ DataflowEngine/test/patches/AdderTest.cs
@@ -1,1 +1,69 @@
-//TODO add missing test
+/*
+* Copyright © 2008 Rodrigo Kumpera, Carlos Villela
+*
+* Permission to use, copy, modify, distribute, and sell this software and its
+* documentation for any purpose is hereby granted without fee, provided that
+* the above copyright notice appear in all copies and that both that
+* copyright notice and this permission notice appear in supporting
+* documentation, and that the name of SuSE not be used in advertising or
+* publicity pertaining to distribution of the software without specific,
+* written prior permission.  SuSE makes no representations about the
+* suitability of this software for any purpose.  It is provided "as is"
+* without express or implied warranty.
+*
+* SuSE DISCLAIMS ALL WARRANTIES WITH REGARD TO THIS SOFTWARE, INCLUDING ALL
+* IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS, IN NO EVENT SHALL SuSE
+* BE LIABLE FOR ANY SPECIAL, INDIRECT OR CONSEQUENTIAL DAMAGES OR ANY DAMAGES
+* WHATSOEVER RESULTING FROM LOSS OF USE, DATA OR PROFITS, WHETHER IN AN ACTION
+* OF CONTRACT, NEGLIGENCE OR OTHER TORTIOUS ACTION, ARISING OUT OF OR IN
+* CONNECTION WITH THE USE OR PERFORMANCE OF THIS SOFTWARE.
+*
+* Author:  Carlos Villela <cv@lixo.org>
+*/
+
+using System;
+using NUnit.Framework;
+using NMock2;
+using Dataflow.Core;
+
+namespace Dataflow.Patches
+{
+	[TestFixture()]
+	public class AdderTest
+	{
+    private Mockery mocks;
+    private IPatchContainer mockPatchContainer;
+
+    [SetUp]
+    public void SetUp() {
+      mocks = new Mockery();
+      mockPatchContainer = mocks.NewMock<IPatchContainer>();
+    }
+
+		[Test()]
+		public void ShouldAddTwoInletsTogether() {
+		  Expect.Once.On(mockPatchContainer).
+                  Method("AddInlet").
+                  With("left").
+                  Will(Return.Value(new Inlet<int>("left", mockPatchContainer, ActivationMode.ActivateOnMessage)));
+
+		  Expect.Once.On(mockPatchContainer).
+                  Method("AddInlet").
+                  With("right").
+                  Will(Return.Value(new Inlet<int>("right", mockPatchContainer, ActivationMode.ActivateOnMessage)));
+
+		  Expect.Once.On(mockPatchContainer).
+                  Method("AddOutlet").
+                  With("result").
+                  Will(Return.Value(new Outlet<int>("result", mockPatchContainer)));
+
+		  Expect.Once.On(mockPatchContainer).
+                  GetProperty("CurrentFrame").
+                  Will(Return.Value(0));
+
+			Adder adder = new Adder();
+			adder.Init(mockPatchContainer);
+			adder.Execute();
+		}
+	}
+}
