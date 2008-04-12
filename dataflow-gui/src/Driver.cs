b11d67c58d0d1e3f14c71a710459b9b659e3373a
Rodrigo Kumpera <kumpera@gmail.com>
2008-04-12 13:15:18 -0300

    Minimal thing that just show a rectangle

---
+++ dataflow-gui/src/Driver.cs
@@ -0,0 +1,19 @@
+using System;
+using Gtk;
+
+
+namespace Dataflow.Gui
+{
+
+public class Driver
+{
+    static void Main ()
+    {
+		Application.Init ();
+		EditorWindow ed = new EditorWindow ();
+		ed.ShowAll ();
+	
+		Application.Run ();
+    }
+}
+}
---
+++ dataflow-gui/src/EditorCanvas.cs
@@ -0,0 +1,29 @@
+using System;
+using Cairo;
+
+namespace Dataflow.Gui
+{
+
+public class EditorCanvas : Gtk.DrawingArea
+{
+    protected override bool OnExposeEvent (Gdk.EventExpose args) {
+		using (Context g = Gdk.CairoHelper.Create (args.Window)) {
+			int x, y, width, height, depth;
+		    GdkWindow.GetGeometry (out x, out y, out width, out height, out depth);
+			Draw (g, x, y, width, height, depth);
+		}
+
+		return true;
+	}
+
+    void Draw (Context cc, int x, int y, int width, int height, int depth) {
+        cc.Color = new Color (0.5, 0.5, 0.5);
+
+        cc.Pattern = new SolidPattern (new Color (1.0, 0, 0), true);
+
+        cc.Rectangle (20, 20, 100, 100);
+        cc.Fill ();      
+    }
+}
+
+}
