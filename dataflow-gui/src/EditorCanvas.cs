eeea15189dff071ed35db41483288040e1492b8b
Rodrigo Kumpera <kumpera@gmail.com>
2008-04-12 19:02:59 -0300

    A full Patch GUI mockup. Building the layout won't be nice

--- dataflow-gui/src/EditorCanvas.cs
+++ dataflow-gui/src/EditorCanvas.cs
@@ -16,6 +16,7 @@ public class EditorCanvas : Gtk.DrawingArea {
 
 	static Color LIGHT_GRAY = new Color(0.8, 0.8, 0.8);
 	static Color MEDIUN_GRAY = new Color(0.6, 0.6, 0.6);
+	static Color DARK_GRAY = new Color(0.2, 0.2, 0.2);
 	static Color BLACK = new Color (0, 0, 0);
 	static Color GREEN = new Color (0.1, 0.8, 0.1);
 
@@ -25,7 +26,7 @@ public class EditorCanvas : Gtk.DrawingArea {
         cc.Paint();
 
 		//Patch envolope
-        cc.Color = MEDIUN_GRAY;
+        cc.Color = LIGHT_GRAY;
         DrawRoundedRectangle(cc, 40, 40, 120, 80, 10);
 		cc.FillPreserve ();
 		cc.LineWidth = 1;
@@ -40,9 +41,52 @@ public class EditorCanvas : Gtk.DrawingArea {
 		cc.Color = BLACK;
 		cc.Stroke ();
 
+		//Header text
 		cc.MoveTo (50, 55);
 		cc.SetFontSize (12);
-		cc.ShowText ("Name");
+		cc.ShowText ("HSL Color");
+
+		DrawInlet(cc, 50, 70, "Hue");
+
+		DrawInlet(cc, 50, 84, "Saturation");
+
+		DrawInlet(cc, 50, 98, "Luminosity");
+
+		DrawInlet(cc, 50, 112, "Alpha");
+
+		DrawOutlet (cc, 150, 70, "Color");
+	}
+
+	void DrawInlet (Context cc, double x, double y, string name) {
+		cc.Color = DARK_GRAY;
+		cc.LineWidth = 1;
+		cc.SetFontSize (10);
+
+		DrawCircle(cc, x, y, 4);
+		cc.Stroke ();
+
+		cc.MoveTo (x + 7, y + 4);
+		cc.ShowText (name);
+	}
+
+	void DrawOutlet (Context cc, double x, double y, string name) {
+		cc.Color = DARK_GRAY;
+		cc.LineWidth = 1;
+		cc.SetFontSize (10);
+
+		DrawCircle(cc, x, y, 4);
+		cc.Stroke ();
+
+		TextExtents te = cc.TextExtents (name);
+		cc.MoveTo (x - 8 - te.Width, y + 4);
+		cc.ShowText (name);
+	}
+
+
+	void DrawCircle (Context cc, double cx, double cy, double radius) {
+		cc.NewPath ();
+        cc.Arc(cx, cy, radius, 0, 2 * Math.PI);
+
 	}
 
 	void DrawPatchHeader (Context cc, double x, double y, double width, double height, double arcRadius) {
