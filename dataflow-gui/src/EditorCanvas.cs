79188acf0b68979aa2846a3bb2fcad5273b05e5c
Rodrigo Kumpera <kumpera@gmail.com>
2008-04-13 18:29:30 -0300

    Drag Support added

--- dataflow-gui/src/EditorCanvas.cs
+++ dataflow-gui/src/EditorCanvas.cs
@@ -63,17 +63,6 @@ internal static class DrawingPrimitives {
 }
 
 public class PatchWidget {
-	String name;
-	String[] inlets;
-	String[] outlets;
-	Context ctx;
-
-	public PatchWidget (String name, String[] inlets, String[] outlets) {
-		this.name = name;
-		this.inlets = inlets;
-		this.outlets = outlets;
-	}
-
 	const int HEADER_HEIGHT = 20;
 	const int FIRST_LINE = HEADER_HEIGHT + 10;
 
@@ -85,37 +74,95 @@ public class PatchWidget {
 	const int PORT_BORDER_SPACING = 17;
 	const int PORT_INNER_SPACING = 8;
 
+	String name;
+	String[] inlets;
+	String[] outlets;
+	Context ctx;
+
+	public double X { get; set; }
+	public double Y { get; set; }
+
+	public double Width { get; set; }
+	public double Height { get; set; }
+
+	public PatchWidget (String name, String[] inlets, String[] outlets) {
+		this.name = name;
+		this.inlets = inlets;
+		this.outlets = outlets;
+	}
+
 	public void Draw (Context ctx, LogHandler log) {
 		this.ctx = ctx;
 		//We must first calculate the layout width based on title size and inlet/outlet sizes
-		double width = CalcWidth ();
-		double height = HEADER_HEIGHT + Math.Max (inlets.Length, outlets.Length) * PORT_HEIGHT + BORDER_ROUND_SIZE;
+		if (Width < 1) {
+			Width = CalcWidth ();
+			Height = HEADER_HEIGHT + Math.Max (inlets.Length, outlets.Length) * PORT_HEIGHT + BORDER_ROUND_SIZE;
 
-		//Patch envolope
+		}
+
+		DrawEnvelope ();
+
+		DrawHeader ();
+
+		inlets.EachWithIndex ((n, idx) => DrawInlet (FIRST_LINE + idx * PORT_HEIGHT, n));
+		outlets.EachWithIndex ((n, idx) => DrawOutlet (FIRST_LINE + idx * PORT_HEIGHT, n));
+	}
+
+	public bool HitTest (double px, double py) {
+		return 	px >= X && px < (X + Width) &&
+				py >= Y && py < (Y + Height);
+	}
+
+	void DrawEnvelope () {
         ctx.Color = Constants.LIGHT_GRAY;
-        DrawingPrimitives.DrawRoundedRectangle(ctx, 0, 0, width, height, BORDER_ROUND_SIZE);
+        DrawingPrimitives.DrawRoundedRectangle(ctx, 0, 0, Width, Height, BORDER_ROUND_SIZE);
 		ctx.FillPreserve ();
 		ctx.LineWidth = 1;
 		ctx.Color = Constants.BLACK;
 		ctx.Stroke ();	
+	}
 
-		//Patch header
+	void DrawHeader () {
+		//Box
 		ctx.Color = Constants.GREEN;
-        DrawingPrimitives.DrawUpRoundedRectangle (ctx, 0, 0, width, HEADER_HEIGHT, BORDER_ROUND_SIZE);
+        DrawingPrimitives.DrawUpRoundedRectangle (ctx, 0, 0, Width, HEADER_HEIGHT, BORDER_ROUND_SIZE);
 		ctx.FillPreserve ();
 		ctx.LineWidth = 1;
 		ctx.Color = Constants.BLACK;
 		ctx.Stroke ();
 
-		//Header text
+		//text
 		ctx.MoveTo (BORDER_ROUND_SIZE, 15);
 		ctx.SetFontSize (HEADER_FONT_SIZE);
-		ctx.ShowText (name);
-
-		inlets.EachWithIndex ((n, idx) => DrawInlet (FIRST_LINE + idx * PORT_HEIGHT, n));
-		outlets.EachWithIndex ((n, idx) => DrawOutlet (FIRST_LINE + idx * PORT_HEIGHT, width, n));
+		ctx.ShowText (this.name);
 	}
 
+	void DrawInlet (double y, string inlet) {
+		ctx.Color = Constants.DARK_GRAY;
+		ctx.LineWidth = 1;
+		ctx.SetFontSize (PORT_FONT_SIZE);
+
+		DrawingPrimitives.DrawCircle(ctx, 10, y, 4);
+		ctx.Stroke ();
+
+		ctx.MoveTo (PORT_BORDER_SPACING, y + 4);
+		ctx.ShowText (inlet);
+	}
+
+	void DrawOutlet (double y, string outlet) {
+		ctx.Color = Constants.DARK_GRAY;
+		ctx.LineWidth = 1;
+		ctx.SetFontSize (PORT_FONT_SIZE);
+
+		DrawingPrimitives.DrawCircle(ctx, Width - 10, y, 4);
+		ctx.Stroke ();
+
+		TextExtents te = ctx.TextExtents (outlet);
+		ctx.MoveTo (Width - (PORT_BORDER_SPACING + 1) - te.Width, y + 4);
+		ctx.ShowText (outlet);
+	}
+
+	//Layout stuff
 	double CalcWidth () {
 		double width = 0;
 		//Header text must not enter the round border area
@@ -144,38 +191,7 @@ public class PatchWidget {
 		TextExtents te = ctx.TextExtents (str);
 		return te.Width;
 	}
-		
-
-	void DrawInlet (double y, string inlet) {
-		ctx.Color = Constants.DARK_GRAY;
-		ctx.LineWidth = 1;
-		ctx.SetFontSize (PORT_FONT_SIZE);
-
-		DrawingPrimitives.DrawCircle(ctx, 10, y, 4);
-		ctx.Stroke ();
-
-		ctx.MoveTo (17, y + 4);
-		ctx.ShowText (inlet);
-	}
-
-	void DrawOutlet (double y, double width, string outlet) {
-		ctx.Color = Constants.DARK_GRAY;
-		ctx.LineWidth = 1;
-		ctx.SetFontSize (PORT_FONT_SIZE);
-
-		DrawingPrimitives.DrawCircle(ctx, width - 10, y, 4);
-		ctx.Stroke ();
-
-		TextExtents te = ctx.TextExtents (outlet);
-		ctx.MoveTo (width - 18 - te.Width, y + 4);
-		ctx.ShowText (outlet);
-	}
-
-
-	public double X { get; set; }
-	public double Y { get; set; }
 }
-
 
 public class EditorCanvas : Gtk.DrawingArea {
 	PatchWidget ui;
@@ -193,17 +209,48 @@ public class EditorCanvas : Gtk.DrawingArea {
 
 	void SetupEvents () {
 		GdkWindow.Events |= Gdk.EventMask.ButtonMotionMask | Gdk.EventMask.ButtonPressMask | Gdk.EventMask.ButtonReleaseMask;
-
-		ButtonPressEvent += (obj, evnt) => LogEvent ("button pressed " + evnt);
-		ButtonReleaseEvent += (obj, evnt) => LogEvent ("button pressed " + evnt);
-		MotionNotifyEvent += (obj, evnt) => LogEvent ("button moved " + evnt);
 	}
+
+	PatchWidget dragPatch;
+	//TODO replace this crap with a Pointer type
+	double ox, oy;
+	double cx, cy;
+
+	protected override bool OnButtonPressEvent (Gdk.EventButton ev) {
+		LogEvent ("button pressed x: " + ev.X + " y " + ev.Y);
+		if (ui.HitTest (ev.X, ev.Y)) { //begin draw sequences
+			dragPatch = ui;
+			cx = ev.X;
+			cy = ev.Y;
+			ox = ui.X;
+			oy = ui.Y;
+		}
+	
+		return false;
+	}
+
+	protected override bool OnButtonReleaseEvent(Gdk.EventButton ev) {
+		//LogEvent ("button release " + ev);
+		dragPatch = null;
+		return false;
+	}
+
+	protected override bool OnMotionNotifyEvent(Gdk.EventMotion ev) {
+		//LogEvent ("motion moved " + ev);
+		if (dragPatch != null) {
+			dragPatch.X = ox + (ev.X - cx);
+			dragPatch.Y = oy + (ev.Y - cy);
+			QueueDraw ();
+		}
+		return false;
+	}
+
 
     protected override bool OnExposeEvent (Gdk.EventExpose args) {
         using(Context cc = Gdk.CairoHelper.Create(args.Window)) {
 			cc.Antialias = Antialias.Subpixel;
 
-			//fill background
+			//clear background
 			cc.Color = Constants.LIGHT_GRAY;
 			cc.Paint();
 
@@ -220,87 +267,6 @@ public class EditorCanvas : Gtk.DrawingArea {
     }
 
 	public LogHandler LogEvent { get; set; }
-/*
-    void Draw(Context cc, int x, int y, int width, int height, int depth) {
-
-		//Header text
-		cc.MoveTo (50, 55);
-		cc.SetFontSize (12);
-		cc.ShowText ("HSL Color");
-
-		DrawInlet(cc, 50, 70, "Hue");
-
-		DrawInlet(cc, 50, 84, "Saturation");
-
-		DrawInlet(cc, 50, 98, "Luminosity");
-
-		DrawInlet(cc, 50, 112, "Alpha");
-
-		DrawOutlet (cc, 150, 70, "Color");
-	}
-
-	void DrawInlet (Context cc, double x, double y, string name) {
-		cc.Color = DARK_GRAY;
-		cc.LineWidth = 1;
-		cc.SetFontSize (10);
-
-		DrawCircle(cc, x, y, 4);
-		cc.Stroke ();
-
-		cc.MoveTo (x + 7, y + 4);
-		cc.ShowText (name);
-	}
-
-	void DrawOutlet (Context cc, double x, double y, string name) {
-		cc.Color = DARK_GRAY;
-		cc.LineWidth = 1;
-		cc.SetFontSize (10);
-
-		DrawCircle(cc, x, y, 4);
-		cc.Stroke ();
-
-		TextExtents te = cc.TextExtents (name);
-		cc.MoveTo (x - 8 - te.Width, y + 4);
-		cc.ShowText (name);
-	}
-
-
-	void DrawCircle (Context cc, double cx, double cy, double radius) {
-		cc.NewPath ();
-        cc.Arc(cx, cy, radius, 0, 2 * Math.PI);
-
-	}
-
-	void DrawPatchHeader (Context cc, double x, double y, double width, double height, double arcRadius) {
-        cc.MoveTo(x + arcRadius, y);
-        cc.LineTo(x + width - arcRadius, y);
-        cc.Arc(x + width - arcRadius, y + arcRadius, arcRadius, -Math.PI / 2 , 0);
-
-        cc.LineTo(x + width, y + height);
-        cc.LineTo(x,  y + height);
-        cc.LineTo(x, y + arcRadius);
-        cc.Arc(x + arcRadius, y + arcRadius, arcRadius, Math.PI, -Math.PI / 2);
-
-        cc.ClosePath();
-	}
-
-
-    void DrawRoundedRectangle (Context cc, double x, double y, double width, double height, double arcRadius) {
-        cc.MoveTo(x + arcRadius, y);
-        cc.LineTo(x + width - arcRadius, y);
-        cc.Arc(x + width - arcRadius, y + arcRadius, arcRadius, -Math.PI / 2 , 0);
-
-        cc.LineTo(x + width, y + height - arcRadius);
-        cc.Arc(x + width - arcRadius, y + height - arcRadius, arcRadius, 0, Math.PI / 2);
-
-        cc.LineTo(x + arcRadius,  y + height);
-        cc.Arc(x + arcRadius, y + height - arcRadius, arcRadius, Math.PI / 2, Math.PI);
-
-        cc.LineTo(x,  y + arcRadius);
-        cc.Arc(x + arcRadius, y + arcRadius, arcRadius, Math.PI, -Math.PI / 2);
-
-        cc.ClosePath();
-    }*/
 }
 
 }
