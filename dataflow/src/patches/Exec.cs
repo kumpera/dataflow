c9473081116958b440109d4a598d1f799cf06cc3
Carlos Villela <cv@lixo.org>
2008-04-18 13:17:02 +0100

    rough sketch of the Exec patch

---
+++ dataflow/src/patches/Exec.cs
@@ -0,0 +1,47 @@
+/*
+* Copyright © 2008 The Dataflow Team
+*
+* See AUTHORS and LICENSE for details.
+*/
+
+using System;
+using System.Diagnostics;
+using Dataflow.Core;
+
+namespace Dataflow.Patches {
+
+  // http://www.go-mono.com/docs/index.aspx?link=N%3ASystem.Diagnostics
+
+[Patch(Name="Exec")]
+public class Exec {
+
+    [Inlet(Name="Arguments")]
+    public Inlet<string> args { get; set; }
+
+    [Inlet(Name="Path")]
+    public Inlet<string> path { get; set; }
+
+    [Inlet(Name="Input")]
+    public Inlet<string> stdin { get; set; }
+
+    [Outlet(Name="Output")]
+    public Outlet<string> stdout { get; set; }
+
+    [Outlet(Name="Error")]
+    public Outlet<string> stderr { get; set; }
+    
+    public void Init(IPatchContainer container) {
+        args = container.AddInlet<string>("Arguments");
+        path = container.AddInlet<string>("Path");
+        stdin = container.AddInlet<string>("Input");
+
+        stdout = container.AddOutlet<string>("Output");
+        stderr = container.AddOutlet<string>("Error");
+    }
+
+    public void Execute() {
+    }
+  
+}
+
+}
